77d14f744741e6dcac026a84bd9912a207cae250
Satwik Agarwal <asatwik218@gmail.com>
2023-07-26 01:26:56 +0530

    boilerplate phone auth working

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import { ConfirmationResult, RecaptchaVerifier } from "@firebase/auth";
+export {};
+declare global {
+  interface Window {
+    recaptchaVerifier: RecaptchaVerifier;
+    confirmationResult: ConfirmationResult;
+  }
+}
